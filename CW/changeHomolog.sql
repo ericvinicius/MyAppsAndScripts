52d3cf14ed2abc7485b1a48717539794eaa40808
ericvinicius <eric.camargo@gmail.com>
2015-08-19 14:47:18 -0300

    Adicionando scpt para popular o banco de homolog com o dump

---
+++ CW/changeHomolog.sql
@@ -0,0 +1,5 @@
+use caelumweb2;
+update Usuario u set u.login = "claudio" where u.id = 1;
+update Usuario u set u.login = "c" where u.id = 5;
+update Usuario u set u.login = "camila" where u.id = 14;
+update Usuario u set u.senha = "14d777febb71c53630e9e843bedbd4d8" where u.id > 0;
